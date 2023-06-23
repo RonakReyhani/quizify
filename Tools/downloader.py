2297aeead51542b5db98769734ec35c9f420cc11
reyhani.ronak@gmail.com <reyhani.ronak@gmail.com>
2023-06-23 20:37:18 +0800

    reader and downloader

---
+++ Tools/downloader.py
@@ -0,0 +1,31 @@
+import requests
+from transformers import Tool
+
+def downloader(url):
+  print(url)
+  # NOTE the stream=True parameter below
+  file_name = url.split('/')[-1]
+  print(file_name)
+  # # file_type=".pdf"
+  # file= '{}{}'.format(file_name, file_type)
+  with requests.get(url, stream=True) as r:
+      r.raise_for_status()
+      with open(file_name, 'wb') as f:
+          for chunk in r.iter_content(chunk_size=8192): 
+              # If you have chunk encoded response uncomment if
+              # and set chunk_size parameter to None.
+              #if chunk: 
+              f.write(chunk)
+  return file_name
+
+class download_file (Tool):
+  name="download_file_tool"
+  description="This is a tool for downloading documents (pdf) from the web. It takes an input named `url` which should be the web url containing the document (pdf). It downloads the document and stores the content of the document in the local file."
+  input=['text']
+  output=['text']
+
+  def __call__(self,url: str):
+    print("url", url)
+    return downloader(url)
+
+download_file_tool =  download_file()
---
+++ Tools/readPDF.py
@@ -0,0 +1,36 @@
+import fitz
+from transformers import Tool
+
+def read_pdf(filename):
+  context = ""
+  
+  # Open the PDF file
+  with fitz.open(f"{filename}") as pdf_file:
+  
+    # Get the number of pages in the PDF file
+    num_pages = pdf_file.page_count
+    
+    # Loop through each page in the PDF file
+    for page_num in range(num_pages):
+      
+      # Get the current page
+      page = pdf_file[page_num]
+      
+      # Get the text from the current page
+      page_text = page.get_text()
+      
+      # Append the text to context
+      context += page_text
+  return context
+
+
+
+class read_file (Tool):
+  name="read_file_tool"
+  description="This is a tool for reading documents (pdf) from the disk. It takes an input named `file_name` which should be the name of the file containing the document (pdf). It reads the document and returns the content of the document as text."
+  input=['text']
+  output=['text']
+  def __call__(self, file_name: str):
+    return read_pdf(file_name)
+
+read_file_tool =  read_file()
